6c8ee09cc0032e34467ce18ace5162ec7d4e68d3
bisham paracha <bishamparacha789@gmail.com>
2024-11-24 16:08:09 +0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/navbar";
 import PageTemplate from "./components/pageTemplate";
 import Landing from "./components/landing";
-import About from "./components/about backup";
+import About from "./components/about";
 import Services from "./components/services";
 import Contact from "./components/contact";
 
